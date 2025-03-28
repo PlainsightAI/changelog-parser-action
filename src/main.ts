249aec87bbfa20c723939d676a8cb1f1d0050b8a
Matt Almenshad <malmenshad@plainsight.ai>
2025-03-28 12:05:37 -0700

    ENG-1113: Update logging

--- src/main.ts
+++ src/main.ts
@@ -5,26 +5,39 @@ async function run(): Promise<void> {
   const path = core.getInput('path') || undefined;
   const version = core.getInput('version') || undefined;
 
-  core.startGroup('Parse CHANGELOG');
   const entry = await new Action().run(version, path);
-  core.info(`Version: "${entry?.version ?? ""}"`);
-  core.info(`  Major: "${entry?.versionMajor ?? ""}"`);
-  core.info(`  Minor: "${entry?.versionMinor ?? ""}"`);
-  core.info(`  Patch: "${entry?.versionPatch ?? ""}"`);
-  core.info(`Date: "${entry?.date ?? ""}"`);
-  core.info(`Status: "${entry?.status ?? ""}"`);
-  core.info(`Description:\n${entry?.description ?? ""}\n`);
+
+  const versionStr = entry?.version ?? "";
+  const major = entry?.versionMajor ?? "";
+  const minor = entry?.versionMinor ?? "";
+  const patch = entry?.versionPatch ?? "";
+  const build = entry?.buildNumber ?? "";
+  const suffix = entry?.suffix ?? "";
+  const date = entry?.date ?? "";
+  const description = entry?.description ?? "";
+  const isProductionRelease = build === "" ? "true" : "false";
+
+  core.startGroup('Parse CHANGELOG');
+  core.info(`Version: "${versionStr}"`);
+  core.info(`  Major: "${major}"`);
+  core.info(`  Minor: "${minor}"`);
+  core.info(`  Patch: "${patch}"`);
+  core.info(`  Build: "${build}"`);
+  core.info(`  Suffix: "${suffix}"`);
+  core.info(`  isProductionRelease: "${isProductionRelease}"`);
+  core.info(`Date: "${date}"`);
+  core.info(`Description:\n${description}\n`);
   core.endGroup();
 
-  core.setOutput('version', entry?.version ?? "");
-  core.setOutput('versionMajor', entry?.versionMajor ?? "");
-  core.setOutput('versionMinor', entry?.versionMinor ?? "");
-  core.setOutput('versionPatch', entry?.versionPatch ?? "");
-  core.setOutput("buildNumber", entry?.buildNumber ?? "");
-  core.setOutput("suffix", entry?.suffix ?? "");
-  core.setOutput('date', entry?.date ?? "");
-  core.setOutput('description', entry?.description ?? "");
-  core.setOutput("isProductionRelease", entry?.buildNumber === undefined ? "true" : "false");
+  core.setOutput('version', versionStr);
+  core.setOutput('versionMajor', major);
+  core.setOutput('versionMinor', minor);
+  core.setOutput('versionPatch', patch);
+  core.setOutput('buildNumber', build);
+  core.setOutput('suffix', suffix);
+  core.setOutput('date', date);
+  core.setOutput('description', description);
+  core.setOutput('isProductionRelease', isProductionRelease);
 }
 
 async function main(): Promise<void> {
